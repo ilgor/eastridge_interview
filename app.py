f03681be61fda58222a1f82226d5282363d70bb0
moto <ilgorbekkuchkarov@Ilgorbeks-MacBook-Pro.local>
2020-02-08 18:02:01 -0500

    Added API /all, Updated POST api to /new

--- app.py
+++ app.py
@@ -3,18 +3,50 @@ from flask import request, jsonify
 from flask_restful import Api, Resource
 from config import app, db
 from models import Invoice, InvoiceItem, InvoiceSchema, InvoiceItemSchema
-
+import pandas as pd
 
 api = Api(app)
+
+
+class InvoiceBasedOnAllParamsListResource(Resource):
+
+    def __init__(self):
+        self.invoice_schema = InvoiceSchema(many=True)
+
+    def get(self):
+        client_name = request.args.get('client_name')
+        date_created = request.args.get('date_created')
+        units = request.args.get('units')
+        description = request.args.get('description')
+        amount = request.args.get('amount')
+
+        kwargs_invoice = {}
+        kwargs_invoice_item = {}
+
+        if client_name:
+            kwargs_invoice['client_name'] = client_name
+        if date_created:
+            kwargs_invoice['date_created'] = pd.to_datetime(request.args.get('date_created'))
+        if units:
+            kwargs_invoice_item['units'] = units
+        if description:
+            kwargs_invoice_item['description'] = description
+        if amount:
+            kwargs_invoice_item['amount'] = amount
+        
+        
+        result = db.session.query(Invoice)\
+            .filter_by(**kwargs_invoice)\
+            .join(InvoiceItem)\
+            .filter_by(**kwargs_invoice_item)
+            
+        return jsonify(self.invoice_schema.dump(result))
+
 
 class InvoiceListResource(Resource):
 
     def __init__(self):
         self.invoice_schema = InvoiceSchema(many=True)
-    
-    def get(self):
-        invoices = Invoice.query.all()
-        return jsonify(self.invoice_schema.dump(invoices))
 
     def post(self):
         new_invoice = Invoice(client_name=request.json['client_name'])
@@ -64,7 +96,10 @@ class InvoiceBasedOnClientNameListResource(Resource):
         db.session.commit()
         return '', 204
 
-api.add_resource(InvoiceListResource, '/')
+
+
+api.add_resource(InvoiceBasedOnAllParamsListResource, '/all')
+api.add_resource(InvoiceListResource, '/new')
 api.add_resource(SingleInvoiceListResource, '/<int:id>')
 api.add_resource(InvoiceBasedOnClientNameListResource, '/client/<name>')
 
