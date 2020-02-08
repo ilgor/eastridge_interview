99c5466a95f6481ea729ed6b924dc062654597be
moto <ilgorbekkuchkarov@Ilgorbeks-MacBook-Pro.local>
2020-02-08 15:23:35 -0500

    GET, DELETE based on ID, POST full invoice

--- app.py
+++ app.py
@@ -1,36 +1,54 @@
 from datetime import datetime
-
 from flask import request, jsonify
 from flask_restful import Api, Resource
-
 from config import app, db
 from models import Invoice, InvoiceItem, InvoiceSchema, InvoiceItemSchema
 
 
 api = Api(app)
 
-
 class InvoiceListResource(Resource):
+
+    def __init__(self):
+        self.invoice_schema = InvoiceSchema(many=True)
+    
     def get(self):
         invoices = Invoice.query.all()
-        invoice_schema = InvoiceSchema(many=True)
-        return jsonify(invoice_schema.dump(invoices))
+        return jsonify(self.invoice_schema.dump(invoices))
 
     def post(self):
-        new_invoice = InvoiceItem(
-            client_name=request.json['client_name']
-        )
-        new_invoice_item = InvoiceItem(
-            units = request.json['units'],
-            description = request.json['description'],
-            amount = request.json['amount'],
-            invoice = new_invoice
-        )
-        db.session.add(new_invoice_item)
+        new_invoice = Invoice(client_name=request.json['client_name'])
+        db.session.add(new_invoice)
+
+        for item in request.json['invoice_items']:
+            new_invoice_item = InvoiceItem(
+                units = item['units'],
+                description = item['description'],
+                amount = item['amount'],
+                invoice = new_invoice
+            )
+            db.session.add(new_invoice_item)
         db.session.commit()
-        return post_schema.dump(new_invoice_item)
+        return '', 204
+
+
+class SingleInvoiceListResource(Resource):
+
+    def __init__(self):
+        self.invoice_schema = InvoiceSchema()
+
+    def get(self, id):
+        invoice = Invoice.query.get_or_404(id)
+        return jsonify(self.invoice_schema.dump(invoice))
+
+    def delete(self, id):
+        invoice = Invoice.query.get_or_404(id)
+        db.session.delete(invoice)
+        db.session.commit()
+        return '', 204
 
 api.add_resource(InvoiceListResource, '/')
+api.add_resource(SingleInvoiceListResource, '/<int:id>')
 
 
 
